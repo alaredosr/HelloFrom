ca8ab808803a756326016c22b60edb7227151027
anrj2 <jos19006@byui.edu>
2021-05-22 20:46:49 -0600

    Change made by Abby

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	System.out.println("Hello From Armando");
 	// please add your hello here
 	System.out.println("Hello Armando, my name is Everett Tsosie.");
+	System.out.println("Hello Armando, my name is Abby.");
     }
 }
